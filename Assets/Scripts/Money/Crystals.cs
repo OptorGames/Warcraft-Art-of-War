81e790e2c5fb469db930c480094bd94b18853dc3
SAShamark <sashamark7777@gmail.com>
2022-08-10 18:18:38 +0300

    Just save+ Improved system of moving units between cells.

---
+++ Assets/Scripts/Money/Crystals.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Money
+{
+    public class Cristal : MoneySystem
+    {
+        private void Awake()
+        {
+            if (!PlayerPrefs.HasKey("Cristals"))
+            {
+                PlayerPrefs.SetInt("Cristals", 0);
+            }
+        }
+
+        protected override void ChangeMoneyNumber(int valueForChange)
+        {
+            var newCoinsValue = PlayerPrefs.GetInt("Coins") - valueForChange;
+            _moneyText.text = newCoinsValue.ToString();
+            PlayerPrefs.SetInt("Coins", newCoinsValue);
+        }
+    }
+}
